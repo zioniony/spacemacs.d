3fa7e7f2f69683be20ab082e5ed611434e5dea86
yangzy <yangzy@belorobo.com>
2019-05-10 13:27:30 +0800

    add key binding for python mode

--- init.el
+++ init.el
@@ -505,6 +505,7 @@ getenv "PYTHONPATH"))))
       "fa" 'beginning-of-defun
       "fe" 'end-of-defun
       "fh" 'python-mark-defun
+      "fr" 'load-full-python-layer
   )
 
   ;; helm-find-files ignored pattern is on helm-boring-file-regexp-list.
@@ -530,6 +531,7 @@ getenv "PYTHONPATH"))))
       (find-alternate-file (buffer-file-name))
       (setq python-mode-hook (list))
       (setq python-backend my-python-backend)
+      (message (format "reloaded: %s." (buffer-file-name)))
       )
     )
 
@@ -538,7 +540,7 @@ getenv "PYTHONPATH"))))
   (setq my-python-mode-hook python-mode-hook)
   (setq python-mode-hook (list))
   (add-hook 'sh-mode-hook 'show-wider-tab)
-  (add-hook 'pyvenv-pre-activate-hooks 'ly/set-python-path)
+  ;;(add-hook 'pyvenv-pre-activate-hooks 'ly/set-python-path)
 
   (defun qiang-comment-dwim-line (&optional arg)
     "Replacement for the comment-dwim command. If no region is selected and current line is not blank and we are not at the end of the line, then comment current line. Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
